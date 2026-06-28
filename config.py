ea23c7c6f1696fcddd3d5205d27ea9ac2a757648
Premalatha S <premalatha1906@gmail.com>
2026-06-28 17:37:35 +0530

    feat: enable database pooling for PostgreSQL connections

--- config.py
+++ config.py
@@ -10,3 +10,9 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_size": 5,
+        "max_overflow": 2,
+        "pool_timeout": 30,
+        "pool_recycle": 1800,
+    }
