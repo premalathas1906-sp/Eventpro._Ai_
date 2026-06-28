de7696f7e37415eec8408126c385fc29cecfb06e
Premalatha S <premalatha1906@gmail.com>
2026-06-28 17:42:34 +0530

    fix: disable debug mode in run.py for production

--- run.py
+++ run.py
@@ -9,6 +9,6 @@ app = create_app()
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True, use_reloader=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, use_reloader=False)
 
 
